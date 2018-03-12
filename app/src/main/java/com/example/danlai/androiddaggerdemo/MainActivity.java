71c832fca90ff7560b208907ddee93739b0e5e89
shendanlai <dlshen@51tniu.com>
2018-03-12 16:22:04 +0800

    update gitignore  最简单的Dagger2可以回滚到这边

--- app/src/main/java/com/example/danlai/androiddaggerdemo/MainActivity.java
+++ app/src/main/java/com/example/danlai/androiddaggerdemo/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.danlai.androiddaggerdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 import com.example.danlai.androiddaggerdemo.simpledagger.componet.DaggerActivityComponent;
 import com.example.danlai.androiddaggerdemo.simpledagger.entity.User;
 
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
+
     @Inject
     User user;
 
@@ -17,11 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DaggerActivityComponent.create().inject(this);
         user.setUserName("O'Neal");
         System.out.println(user.toString());
-        DaggerActivityComponent.create().inject(this);
     }
 }
 
-//        System.out.println(group.toString());
 
